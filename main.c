017602c2d481d673e9b9206537a70869b5f355cc
lydiamendjour <155400932+lydiamendjour@users.noreply.github.com>
2024-01-12 20:11:49 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,22 @@
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertNode(int data) {
+    Node* newNode = createNode(data);
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
